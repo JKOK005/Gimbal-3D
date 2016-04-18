ff6bfc71bf2724a5dbbbc1407eede9323e29c1f1
Johan <JKOK005@e.ntu.edu.sg>
2016-04-18 16:36:56 -0700

    Commit before merge

--- Arduino/motor_control/motor_control.ino
+++ Arduino/motor_control/motor_control.ino
@@ -22,7 +22,7 @@ const word pwmPitchPin = 11;        // pwm reading Pin
 const word pwmRollPin = 12;
 const word pwmYawPin = 13;
 
-const word gap = 20;
+const word gap = 10;
 const float gimbalMaxReading = 919;
 const float angleResolution = 0.05;     // Angular resolution across 2*pi
 
@@ -43,7 +43,7 @@ int rollStepA = 0; int rollStepB = (int) (arraySize /3); int rollStepC = (int) (
 int yawStepA = 0; int yawStepB = (int) (arraySize /3); int yawStepC = (int) (arraySize*2 /3);   // Stepping sequence for pitch motor
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   pinMode(13, OUTPUT);
 //  Serial.print(angleResolution);
   }
@@ -53,17 +53,17 @@ void loop() {
   // If no input yet, wait till something is available
   
   if(Serial.available() > 0){
-    signalAvailable();
+    signalAvailable();            // If signal received -> Light up LED
     memset(container, 0, sizeof(container));
     String str = Serial.readStringUntil('@');
     str.toCharArray(container, sizeof(container));
     
     discardReading();
     parseReading(container);
-    
-    Serial.print(pitch_err); Serial.print(" "); Serial.print(roll_err); Serial.print(" ");
-    Serial.println(yaw_err);
-//    movePitchMotor();         // Motor correction
+//    
+//    Serial.print(pitch_err); Serial.print(" "); Serial.print(roll_err); Serial.print(" ");
+//    Serial.println(yaw_err);
+    movePitchMotor();         // Motor correction
 //    moveRollMotor();
 //    moveYawMotor();
    }
@@ -138,7 +138,7 @@ void discardReading(){
   }
 
 void movePitchMotor(){
-  // Function to move the motor to correct for pitch, roll, yaw errors
+  // Function to move the motor to correct for pitch errors
   float currentAnglePrev, err1, err2, err3;
   float currentAngleNow = readCurrentAngle(pwmPitchPin);
   float desiredAngle = currentAngleNow - pitch_err;
@@ -146,8 +146,8 @@ void movePitchMotor(){
 
   if (desiredAngle < 0){desiredAngle = 2*pi + desiredAngle;}
   else if (desiredAngle >= 2*pi){desiredAngle = desiredAngle - 2*pi;}
-  if(pitch_err > 0){increment = 1;}            // Move CW 
-  else{increment = -1;}
+  if(pitch_err > 0){increment = -1;}            // Move CW 
+  else{increment = 1;}
  
 //  while((abs(currentAngleNow - desiredAngle) >= angleResolution)){
     while(abs(desiredAngle - currentAngleNow) >= angleResolution){
@@ -165,32 +165,45 @@ void movePitchMotor(){
       err1 = increment*(desiredAngle - currentAnglePrev);
       err2 = increment*(currentAngleNow - currentAnglePrev);
       err3 = increment*(currentAngleNow - desiredAngle);
-      Serial.print(pitch_err);Serial.print(" "); Serial.print(currentAngleNow - desiredAngle);Serial.println(" ");
-      Serial.print(currentAnglePrev);Serial.print(" "); Serial.print(currentAngleNow);Serial.print(" "); Serial.println(currentAngleNow - currentAnglePrev);
-      Serial.println(" ");
+//      Serial.print(yaw_err);Serial.print(" "); Serial.print(currentAngleNow - desiredAngle);Serial.println(" ");
+//      Serial.print(currentAnglePrev);Serial.print(" "); Serial.print(currentAngleNow);Serial.print(" "); Serial.println(currentAngleNow - currentAnglePrev);
+//      Serial.println(" ");
       delay(10);
     }
   }
 
 void moveRollMotor(){
-  // Function to move the mmotor to correct for pitch, roll, yaw errors
-  float currentAngle = readCurrentAngle(pwmRollPin);
-  float deltaAngle = roll_err - currentAngle;
-  Serial.println(deltaAngle);
-  while(abs(deltaAngle) > angleResolution){
-    if(deltaAngle > 0){increment = 1;}    // Move CW 
-    else{increment = -1;}
+  // Function to move the mmotor to correct for roll errors
+  float currentAnglePrev, err1, err2, err3;
+  float currentAngleNow = readCurrentAngle(pwmRollPin);
+  float desiredAngle = currentAngleNow - roll_err;
+  err1 = -1; err2 = 0; err3 = -1;
+
+  if (desiredAngle < 0){desiredAngle = 2*pi + desiredAngle;}
+  else if (desiredAngle >= 2*pi){desiredAngle = desiredAngle - 2*pi;}
+  if(roll_err > 0){increment = -1;}            // Move CW 
+  else{increment = 1;}
+ 
+//  while((abs(currentAngleNow - desiredAngle) >= angleResolution)){
+    while(abs(desiredAngle - currentAngleNow) >= angleResolution){
+      if(cos(err1) >= cos(err2) && cos(err3) >= cos(err2)) break;  
+      rollStepA = checkLimits(rollStepA + gap*increment);   
+      rollStepB = checkLimits(rollStepB + gap*increment);   
+      rollStepC = checkLimits(rollStepC + gap*increment);
+      currentAnglePrev = currentAngleNow;
       
-    rollStepA = checkLimits(rollStepA + gap*increment);   
-    rollStepB = checkLimits(rollStepB + gap*increment);   
-    rollStepC = checkLimits(rollStepC + gap*increment);
-  
-    analogWrite(rollMotorPin1, pwmSin[rollStepA]);    // Move the pitch motor
-    analogWrite(rollMotorPin2, pwmSin[rollStepB]);
-    analogWrite(rollMotorPin3, pwmSin[rollStepC]);
-    
-    currentAngle = readCurrentAngle(pwmRollPin);                    // Resample reading
-    deltaAngle = roll_err - currentAngle;
+      analogWrite(rollMotorPin1, pwmSin[rollStepA]);    // Move the roll motor
+      analogWrite(rollMotorPin2, pwmSin[rollStepB]);
+      analogWrite(rollMotorPin3, pwmSin[rollStepC]);
+      
+      currentAngleNow = readCurrentAngle(pwmRollPin);                    // Resample reading
+      err1 = increment*(desiredAngle - currentAnglePrev);
+      err2 = increment*(currentAngleNow - currentAnglePrev);
+      err3 = increment*(currentAngleNow - desiredAngle);
+      Serial.print(roll_err);Serial.print(" "); Serial.print(desiredAngle);Serial.println(" ");
+      Serial.print(currentAnglePrev);Serial.print(" "); Serial.print(currentAngleNow);Serial.print(" "); Serial.println(currentAngleNow - currentAnglePrev);
+      Serial.println(" ");
+      delay(10);
     }
   }
   
--- test/passing_message.py
+++ test/passing_message.py
@@ -1,7 +1,7 @@
 import serial
 import time
 
-ser = serial.Serial("COM3", baudrate = 9600, timeout = 1)
+ser = serial.Serial("COM3", baudrate = 115200, timeout = 1)
 time.sleep(1)
 switcher = True
 # q = ' '
@@ -10,21 +10,20 @@ counter = 0
 try:
 	for i in range(50):
 		now = time.time()
-		if(switcher):
-			ser.write(bytes('P:0.409;R:2.938;Y:3.657@', 'utf-8'))		# MUST limit float to 3 dp and in Radians
+		if(not switcher):
+			ser.write(bytes('P:1.111;R:1.111;Y:1.111@', 'utf-8'))		# MUST limit float to 3 dp and in Radians
 
 		else:
-			ser.write(bytes('P:-0.406;R:8.125;Y:-6.594@', 'utf-8'))	
+			ser.write(bytes('P:2.222;R:2.222;Y:-2.222@', 'utf-8'))	
 
 		counter += 1
-		R = ser.readline()
+		# R = ser.readline()
 		ser.flush()		
-		print(ser.in_waiting)
-		print(str(R))		
-		print(str(counter) + " " + str(time.time()-now))
-		time.sleep(0.1)
-		# ser.flush()
-		switcher = not switcher
+		# print(ser.in_waiting)
+		# print(str(R))		
+		# print(str(counter) + " " + str(time.time()-now))
+		time.sleep(0.5)
+		# switcher = not switcher
 
 	while(True):
 		# q = input("type")
@@ -32,7 +31,6 @@ try:
 		# 	ser.write(b'P:1.111;R:5.555;Y:8.888@')
 		print("Im waiting")
 		R = ser.readline()
-		ser.flush()
 		print(ser.in_waiting)
 		print(str(R))
 		time.sleep(1)
